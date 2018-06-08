6c19fcb45d6101daabad409d2c5bcfc0fc4b1d2c
Andrey Popov <andpop@mail.ru>
2018-06-09 00:09:29 +0300

    Переделываю БЭМ-классы

--- src/assets/styles/variables.js
+++ src/assets/styles/variables.js
@@ -1,12 +1,19 @@
 module.exports = {
   bgcolor     : "#223b65",
   bgwelcome   : "#1c1f29",
-  "light-blue": "#004cd1",
-  "dark-blue" : "#043ea3",
-  white       : "#efeee9",
-  beige       : "#eeebdf",
-  green       : "#16c2aa",
-  "text-color": "#455a64",
-  grey        : "#373e42",
-  "light-grey": "#dfdcd5"
+  "light-blue": "#0548be",
+  "dark-blue" : "#083c96",
+  "text-color": "#474b57"
 };
+// module.exports = {
+//   bgcolor     : "#223b65",
+//   bgwelcome   : "#1c1f29",
+//   "light-blue": "#004cd1",
+//   "dark-blue" : "#043ea3",
+//   white       : "#efeee9",
+//   beige       : "#eeebdf",
+//   green       : "#16c2aa",
+//   "text-color": "#455a64",
+//   grey        : "#373e42",
+//   "light-grey": "#dfdcd5"
+// };

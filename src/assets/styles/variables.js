e856849e926c82b352994fd478609efe40326043
Andrey Popov <andpop@mail.ru>
2018-06-11 15:42:42 +0300

    Страница about: нарисовал кружки для скилов

--- src/assets/styles/variables.js
+++ src/assets/styles/variables.js
@@ -5,7 +5,9 @@ module.exports = {
   "dark-blue"    : "#083c96",
   "text-color"   : "#474b57",
   "bgabout-left" : "#f2f0e8",
-  "bgabout-right": "#eae8e2"
+  "bgabout-right": "#eae8e2",
+  "circle-gray"  : "#dfdcd5",
+  "circle-blue"  : "#004cd1"
 };
 // module.exports = {
 //   bgcolor     : "#223b65",

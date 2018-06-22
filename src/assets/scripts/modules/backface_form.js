419d761a865903a17039da1511d7fef615d23ed1
Andrey Popov <andpop@mail.ru>
2018-06-22 07:13:14 +0300

    Делаю крутящуюся плашку на главной странице

--- src/assets/scripts/modules/backface_form.js
+++ src/assets/scripts/modules/backface_form.js
@@ -1,17 +1,38 @@
 console.log("In backface_form.js");
 const authorizeLink = document.querySelector(".authorize-btn__link");
 const mainPageLink = document.getElementById("mainpage-link");
+const backface = document.querySelector(".backface");
+const front = document.querySelector(".front");
+const welcome = document.querySelector(".welcome");
+const backfaceContainer = document.querySelector(".backface-container");
 
+// Переворачиваем плашку, показываем оборотную сторону - форму авторизации
 authorizeLink.addEventListener("click", e => {
-  const backface = document.querySelector(".backface");
-  const front = document.querySelector(".front");
-
   e.preventDefault();
+  e.stopPropagation();
   backface.style.transform = "rotateY(180deg)";
   front.style.zIndex = "0";
+  authorizeLink.style.display = "none";
 });
 
+// Возвращаем плашку в исходное положение
 mainPageLink.addEventListener("click", e => {
   e.preventDefault();
-  console.log("Нажали");
+  e.stopPropagation();
+  backface.style.transform = "";
+  front.style.zIndex = "";
+  authorizeLink.style.display = "";
+});
+
+backfaceContainer.addEventListener("click", e => {
+  e.stopPropagation();
+});
+
+welcome.addEventListener("click", e => {
+  if (front.style.zIndex === "0") {
+    backface.style.transform = "";
+    front.style.zIndex = "";
+    authorizeLink.style.display = "";
+  }
+  console.log(e.target);
 });

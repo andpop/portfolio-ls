d2d1bd5d5518f1b8e348402286c0fb9092b955df
Andrey Popov <andpop@mail.ru>
2018-06-30 12:39:26 +0300

    Делаю слайдер

--- src/assets/scripts/modules/slider.js
+++ src/assets/scripts/modules/slider.js
@@ -2,18 +2,42 @@ console.log("In slider.js");
 import Vue from "vue";
 
 const info = {
-  template: "#slider-info"
+  template: "#slider-info",
+  props: {
+    work: Object
+  }
 };
 
 const display = {
-  template: "#slider-display"
+  template: "#slider-display",
+  props: {
+    work: Object
+  }
 };
 
 const buttons = {
-  template: "#slider-buttons"
+  template: "#slider-buttons",
+  props: {
+    work: Array,
+    currentIndex: Number
+  }
 };
 
 new Vue({
   el: "#slider-component",
+  components: {
+    info,
+    display,
+    buttons
+  },
+  data: {
+    works: [],
+    currentWork: {},
+    currentIndex: 0
+  },
+  created() {
+    this.works = require("../../data/works.json");
+    this.currentWork = this.works[0];
+  },
   template: "#slider"
 });

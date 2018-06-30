d80397f2f976c239d825a6e47438abcc546fff2c
Andrey Popov <andpop@mail.ru>
2018-06-30 10:27:39 +0300

    Начал делать слайдер

--- src/assets/scripts/modules/slider.js
+++ src/assets/scripts/modules/slider.js
@@ -1,1 +1,19 @@
 console.log("In slider.js");
+import Vue from "vue";
+
+const info = {
+  template: "#slider-info"
+};
+
+const display = {
+  template: "#slider-display"
+};
+
+const buttons = {
+  template: "#slider-buttons"
+};
+
+new Vue({
+  el: "#slider-component",
+  template: "#slider"
+});

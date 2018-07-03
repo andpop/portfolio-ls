fb4d27f5b708d062232647d4bda57d09e34fa541
Andrey Popov <andpop@mail.ru>
2018-07-04 00:47:15 +0300

    Подключил axios для AJAX-запросов

--- src/assets/scripts/modules/valid-form.js
+++ src/assets/scripts/modules/valid-form.js
@@ -1,5 +1,6 @@
 // console.log("In valid-form.js");
 import Vue from "vue";
+import axios from "axios";
 
 new Vue({
   el: "#auth-form",
@@ -41,6 +42,12 @@ new Vue({
         return;
       // Форма заполнена корректно - отсылаем данные на сервер
       console.log("AJAX need");
+      axios
+        .get("http://webdev-api.loftschool.com/user?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOjMsImlzcyI6Imh0dHA6Ly93ZWJkZXYtYXBpLmxvZnRzY2hvb2wuY29tL2xvZ2luIiwiaWF0IjoxNTMwNjQyODU4LCJleHAiOjE1MzA2NjA4NTgsIm5iZiI6MTUzMDY0Mjg1OCwianRpIjoiNElSUG1FRTdKWW9zM1JDQSJ9.UuZ-mISka-vtCtcb9jxJzadekTsLzLyYiixeijEQs80")
+        .then(function(response) {
+          // handle success
+          console.log(response);
+        });
     },
     loginFocus() {
       this.invalidLogin = false;

6785e1337840d93acd220471adf5951ea493f1fa
Andrey Popov <andpop@mail.ru>
2018-07-05 20:01:10 +0300

    Начал делать админ-панель (SPA)

--- src/admin/App.vue
+++ src/admin/App.vue
@@ -10,4 +10,13 @@ export default {
 };
 </script>
 <style>
+@import url("https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&subset=cyrillic");
+body {
+  font-family: "Roboto", Helvetica, sans-serif;
+  font-size: 16px;
+  line-height: 1.42;
+  color: #474b57;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
 </style>

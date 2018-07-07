d67cbcbaa043d7d32bfd16b0e27f71deacf026f3
Andrey Popov <andpop@mail.ru>
2018-07-07 13:31:23 +0300

    Начал делать параллакс

--- src/assets/scripts/modules/parallax.js
+++ src/assets/scripts/modules/parallax.js
@@ -1,11 +1,12 @@
-// console.log("In parallax.js");
-// const mountains = document.querySelector(".mountains");
+console.log("In parallax.js");
+const mountains = document.querySelector(".mountains");
 
-// document.addEventListener("mousemove", e => {
-//   const speed = 60;
-//   const pos_x = `${e.pageX / speed}px`;
-//   const pos_y = `${e.pagey / speed}px`;
-//   mountains.style.transform = `translate3d(${pos_x}, ${pos_y}, 0)`;
+document.addEventListener("mousemove", e => {
+  const speed = 60;
+  const pos_x = `${e.pageX / speed}px`;
+  const pos_y = `${e.pageY / speed}px`;
+  mountains.style.transform = `translate3d(${pos_x}, ${pos_y}, 0)`;
 
-//   console.log(e.pageX, e.pageY);
-// });
+  // console.log(e.pageX, e.pageY);
+  console.log(pos_x, pos_y);
+});

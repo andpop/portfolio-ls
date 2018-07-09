21fc9ef034cd24b2610923aa00807a89e60b4e03
Andrey Popov <andpop@mail.ru>
2018-07-09 19:16:42 +0300

    Сделал загрузку в блог данных из внешнего файла

--- src/assets/scripts/modules/posts.js
+++ src/assets/scripts/modules/posts.js
@@ -10,5 +10,18 @@ new Vue({
     this.posts = require("../../data/posts.json");
     console.log(this.posts);
   },
+  methods: {
+    stringDate(unixDate) {
+      const date = new Date(unixDate * 1000);
+      const year = date.getFullYear();
+      const month = zeroFill(date.getMonth() + 1, 2);
+      const day = zeroFill(date.getDate(), 2);
+      return `${day}.${month}.${year}`;
+
+      function zeroFill(num, len) {
+        return (Array(len).join("0") + num).slice(-len);
+      }
+    }
+  },
   template: "#posts"
 });
